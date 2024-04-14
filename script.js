f42a52db1ae9fa7d3b7a172353f24574536a28f3
LordofGhost <134922046+LordofGhost@users.noreply.github.com>
2024-04-14 10:25:14 +0200

    fix layout

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 let num1, num2, operator;
+const lastCalc = document.querySelector('#lastCalc')
+const result = document.querySelector('#result')
 
+// select all input button and add a function to them
+const buttons = document.querySelectorAll('button')
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        console.log(button.textContent)
+    });
+});
+
+// goes through the input and analyze it, decides how to react on it
 function operate (num1, num2, operator) {
     switch (operator) {
         case '+':

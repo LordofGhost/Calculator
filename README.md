c5150b957d0f2f949d9ab93e09e097db677456b7
LordofGhost <134922046+LordofGhost@users.noreply.github.com>
2024-04-14 16:24:11 +0200

    finishing better logic

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Calculator
 
 Icons: flaction.com
+
+Pseudo Code:
+
+Ask for first number
+store that number
+Ask for operant
+store it
+Ask for second number
+store it
+on = or another operant calculate it
+display the result and show the last operation on the top with an = sign
--- script.js
+++ script.js
@@ -2,68 +2,68 @@
 const buttons = document.querySelectorAll('button')
 buttons.forEach(button => {
     button.addEventListener('click', () => {
-        inputHandler(button.id, button.textContent)
+        inputHandler(button.id, button.textContent, button.className)
     });
 });
 
-function inputHandler (id, content) {
-    let num1, num2, operator;
+function inputHandler (id, content, className) {
+    const display = document.querySelector('#result')
+    let isOperatorPressed = false; // this variable is to check if already a operator is in the calculation
     switch (id) {
         case 'buttonClear':
-            displayOnScreen(clear);
+            displayOnScreen('clear');
             break;
         case 'backspace':
+            display.textContent = display.textContent.slice(0, -1);
             break;
         case 'buttonEqual':
-            operate(num1, num2, operator);
+            operate(display.textContent);
             break;
-        case 'positivNegativ':
+        case 'negativ':
+            display.textContent += '-'
+            break;
+        case 'squareRoot':
+            display.textContent += content;
+            operate(display.textContent)
             break;
         default:
+            display.textContent += content;
+            if (isOperatorPressed) {
+                operate(display.textContent);
+                isOperatorPressed = false;
+            }
+            if (className = 'operator') isOperatorPressed = true;
             break;
     }
 }
 
 // goes through the input and analyze it, decides how to react on it
-function operate (num1, num2, operator) {
-    switch (operator) {
+function operate (string) {
+    const arr = string.split(' ')
+    switch (arr[1]) {
         case '+':
-            add(num1, num2)
+            displayOnScreen('result', parseFloat(arr[0]) + parseFloat(arr[2]));
             break;
         case '-':
-            subtract(num1, num2)
+            displayOnScreen('result', parseFloat(arr[0]) - parseFloat(arr[2]));
             break;
-        case '*':
-            multiply(num1, num2)
+        case 'x':
+            displayOnScreen('result', parseFloat(arr[0]) * parseFloat(arr[2]));
             break;
         case '/':
-            divide(num1, num2)
-            break;   
-        default:
+            displayOnScreen('result', parseFloat(arr[0]) / parseFloat(arr[2]));
+            break;
+        case '√':
+            displayOnScreen('result', Math.sqrt(parseFloat(arr[0])));
+            break;
+        case '^':
+            displayOnScreen('result', parseFloat(arr[0]) ** parseFloat(arr[2]));
             break;
     }
 }
 
-// calculations
-
-function add (num1, num2) {
-    return num1 + num2;
-}
-
-function subtract (num1, num2) {
-    return num1 - num2;
-}
-
-function multiply (num1, num2) {
-    return num1 * num2;
-}
-
-function divide (num1, num2) {
-    return num1 / num2;
-}
-
 // inderact with the display
-function displayOnScreen (type, content) {
+function displayOnScreen(type, content) {
     const lastCalc = document.querySelector('#lastCalc')
     const result = document.querySelector('#result')
     if (type == 'number') { // adds a number to calculation

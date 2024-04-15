9c0af9434d1de5718be45665fc7a82c0d9269917
LordofGhost <134922046+LordofGhost@users.noreply.github.com>
2024-04-15 17:11:06 +0200

    rewrote hole logic

--- README.md
+++ README.md
@@ -4,11 +4,4 @@ Icons: flaction.com
 
 Pseudo Code:
 
-Ask for first number
-store that number
-Ask for operant
-store it
-Ask for second number
-store it
-on = or another operant calculate it
-display the result and show the last operation on the top with an = sign
+Logic based on before or after operator
--- script.js
+++ script.js
@@ -1,63 +1,72 @@
-// select all input button and add a function to them
-const buttons = document.querySelectorAll('button')
-buttons.forEach(button => {
+let currentNumber = '', lastNumber = '', operant = '';
+
+// select all input buttons for numbers
+const numberButtons = document.querySelectorAll('.number');
+numberButtons.forEach(button => {
     button.addEventListener('click', () => {
-        inputHandler(button.id, button.textContent, button.className)
+        // add number to string
+        currentNumber += button.textContent;
+        displayOnScreen('number', currentNumber);
     });
 });
 
-function inputHandler (id, content, className) {
-    const display = document.querySelector('#result')
-    let isOperatorPressed = false; // this variable is to check if already a operator is in the calculation
-    switch (id) {
-        case 'buttonClear':
-            displayOnScreen('clear');
-            break;
-        case 'backspace':
-            display.textContent = display.textContent.slice(0, -1);
-            break;
-        case 'buttonEqual':
-            operate(display.textContent);
-            break;
-        case 'negativ':
-            display.textContent += '-'
-            break;
-        case 'squareRoot':
-            display.textContent += content;
-            operate(display.textContent)
-            break;
-        default:
-            display.textContent += content;
-            if (isOperatorPressed) {
-                operate(display.textContent);
-                isOperatorPressed = false;
-            }
-            if (className = 'operator') isOperatorPressed = true;
-            break;
-    }
-}
+// select all input buttons for operators
+const operatorButtons = document.querySelectorAll('.operator');
+operatorButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        if (operant != '') { // before a second operator can be added to the calculation the last one mus be calculated
+
+        } else {
+            // make space for the second number
+            lastNumber = currentNumber;
+            currentNumber = '';
+            // store the oprator
+            operant = button.textContent;
+        }   
+    });
+});
+
+// select all input buttons for modefier
+const modeButtons = document.querySelectorAll('.mode');
+modeButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        switch (button.id) {
+            case 'buttonClear':
+                break;
+            case 'backspace':
+                break;
+        }
+    });
+});
+
+const negativPositiv = document.querySelector('#negativPositiv');
+negativPositiv.addEventListener('click', () => {
+    currentNumber *= -1;
+    displayOnScreen('number', currentNumber);
+});
+
+
 
 // goes through the input and analyze it, decides how to react on it
-function operate (string) {
-    const arr = string.split(' ')
-    switch (arr[1]) {
+function calculate (val1, val2, operant) {
+    switch (operant) {
         case '+':
-            displayOnScreen('result', parseFloat(arr[0]) + parseFloat(arr[2]));
+            displayOnScreen('result', val1 + val2);
             break;
         case '-':
-            displayOnScreen('result', parseFloat(arr[0]) - parseFloat(arr[2]));
+            displayOnScreen('result', val1 - val2);
             break;
         case 'x':
-            displayOnScreen('result', parseFloat(arr[0]) * parseFloat(arr[2]));
+            displayOnScreen('result', val1 * val2);
             break;
         case '/':
-            displayOnScreen('result', parseFloat(arr[0]) / parseFloat(arr[2]));
+            displayOnScreen('result', val1 / val2);
             break;
         case '√':
-            displayOnScreen('result', Math.sqrt(parseFloat(arr[0])));
+            displayOnScreen('result', Math.sqrt(val1));
             break;
         case '^':
-            displayOnScreen('result', parseFloat(arr[0]) ** parseFloat(arr[2]));
+            displayOnScreen('result', val1 ** val2);
             break;
     }
 }
@@ -67,12 +76,16 @@ function displayOnScreen(type, content) {
     const lastCalc = document.querySelector('#lastCalc')
     const result = document.querySelector('#result')
     if (type == 'number') { // adds a number to calculation
-        result.textContent += content;
-    } else if (type == 'result') { // prints the result and shifts the calculation to the top
-        lastCalc.textContent = result.textContent;
         result.textContent = content;
+    } else if (type == 'result') { // prints the result and shifts the calculation to the top
+        // lastCalc.textContent = result.textContent;
+        result.textContent = content.toFixed(2);
     } else if (type == 'clear') { // clears the display
-        lastCalc.textContent = '';
-        result.textContent = '';
+        lastCalc.textContent = '0';
+        result.textContent = '0';
     }
+}
+
+function reset() {
+
 }
